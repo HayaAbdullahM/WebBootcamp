05f2117b030ad26dec33de141bf49dc54c785c47
HayaAbduallah <haya.almossaeed@gmail.com>
2022-11-25 18:58:33 +0300

    answer for L4

--- lecture_04/app.js
+++ lecture_04/app.js
@@ -1,10 +1,32 @@
-
-const selectButton = document.getElementById('name') ; 
-
-selectButton.addEventListener('click', () => {
-
-    const URL = `https://api.aladhan.com/v1/timingsByCity?country=Saudi+Arabia&city=$value`
+const selectButton = document.getElementById("name");
 
 
-    console.log(selectButton.value)
+
+selectButton.addEventListener('change', () => {
+    getTiming(selectButton.value)
 })
+
+async function getTiming(city) {
+    const response = await fetch(`https://api.aladhan.com/v1/timingsByCity?country=Saudi+Arabia&city=${city}`)
+    const json = await response.json() ; 
+    const {Fajr, Dhuhr, Asr, Maghrib, Isha} = json.data.timings ;
+
+    const fajirTableData = document.getElementById('Fajr') ; 
+    fajirTableData.innerHTML = Fajr ; 
+
+    // ! OR 
+
+    document.getElementById('Fajr').innerHTML = Fajr
+
+
+    document.getElementById('Zuhr').innerHTML = Dhuhr
+    document.getElementById('Asr').innerHTML = Asr
+    document.getElementById('Maghrib').innerHTML = Maghrib
+    document.getElementById('Isha').innerHTML = Isha
+}
+
+
+getTiming(selectButton.value)
+
+
+// Curson Parking : 
